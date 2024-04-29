7a009371c91a4002bb14592da92d87255ce7af51
Doritoso.o <meyesblue@gmail.com>
2024-04-28 22:26:22 -0400

    uwu #2

--- apiferremas/settings.py
+++ apiferremas/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -75,9 +78,16 @@ WSGI_APPLICATION = 'apiferremas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/orcl',
+        'USER': 'ferremas',
+        'PASSWORD': 'ferremas',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
 
 
