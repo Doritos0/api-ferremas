f6521af242bd701d12b7489d909bd68e9607db9a
Doritos0 <meyesblue@gmail.com>
2024-05-06 12:27:59 -0400

    uwu #3

--- core/models.py
+++ core/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
-
-class Productos(models.Model):
+class TipoProducto(models.Model):
     TipoProducto = [
         (1, 'Herramientas Manuales'),#1- Herramientas Manuales
         (2, 'Materiales Basicos'),#2- Materiales Basicos
@@ -12,11 +11,24 @@ class Productos(models.Model):
         (5, 'Fijaciones y Adhesivos'), #5-Fijaciones y Adhesivos
         (6, 'Equipos de Medicion'), #6-Equipos de Medicion
     ]
+    id_tipo = models.IntegerField(choices=TipoProducto, primary_key=True)
+
+    def __str__(self):
+        for tipo_id, tipo_nombre in self.TipoProducto:
+            if tipo_id == self.id_tipo:
+                return tipo_nombre
+        return str(self.id_tipo)
+
+ #   def __str__(self):
+    #    return str(self.tipo_nombre)
+
+
+class Productos(models.Model):
     id_producto = models.AutoField(primary_key=True)
-    tipo_producto = models.IntegerField(choices=TipoProducto)
     nombre = models.CharField(max_length=25, unique=True)
     precio = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(99999999)])
     stock = models.IntegerField()
+    id_tipo = models.ForeignKey(TipoProducto, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
