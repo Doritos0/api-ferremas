46bca2d56a41eaf7ffbda3ed114aabb784adb3ef
Doritoso.o <meyesblue@gmail.com>
2024-05-15 15:51:14 -0400

    uwu #10 //SIMPLIFICACION TABLA PRECIOS MANEJO DE FECHAS MANUALMENTE

--- core/models.py
+++ core/models.py
@@ -41,44 +41,12 @@ class Precio(models.Model):
     id_precio = models.AutoField(primary_key=True)
     id_producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     fec_ini = models.DateField()
-    fec_ter = models.DateField(blank=True, null=True, editable=False)
+    fec_ter = models.DateField()
     precio = models.IntegerField()
 
     def __str__(self):
         return f"Precio {self.fec_ini} termino {self.fec_ter}"
-    
-    def clean(self):
-        # Verificamos si la fecha de inicio es mayor que la fecha de inicio de la entrada anterior
-        if self.fec_ini:
-            entradas_anteriores = Precio.objects.filter(
-                id_producto=self.id_producto,
-                fec_ini__lt=self.fec_ini
-            ).order_by('-fec_ini')
 
-            if entradas_anteriores.exists():
-                ultima_entrada_anterior = entradas_anteriores.first()
-                print("💚 ESTA FECHA ",self.fec_ini," NO DEBERIA SER MAYOR A ",ultima_entrada_anterior.fec_ter," ID ",ultima_entrada_anterior.id_precio)
-                if ultima_entrada_anterior.fec_ter == self.fec_ini:
-                    raise ValidationError("Ya hay un precio para esta fecha")
-                elif ultima_entrada_anterior.fec_ter is None or self.fec_ini >= ultima_entrada_anterior.fec_ter:
-                    print("💙 FUNCIONA")
-                else:
-                    raise ValidationError("La fecha de inicio debe ser posterior a la última entrada")
-
-    def save(self, *args, **kwargs):
-        if self.fec_ini:
-            entradas_anteriores = Precio.objects.filter(
-                id_producto=self.id_producto,
-                fec_ini__lt=self.fec_ini
-            ).order_by('-fec_ini')
-
-            if entradas_anteriores.exists():
-                ultima_entrada_anterior = entradas_anteriores.first()
-                ultima_entrada_anterior.fec_ter = self.fec_ini
-                ultima_entrada_anterior.save()
-
-        super(Precio, self).save(*args, **kwargs)
-        
 class Cliente(models.Model):
     rut_cliente = models.CharField(max_length=9, primary_key=True)
     nombre = models.CharField(max_length=100)
