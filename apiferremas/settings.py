8d85aeed5deb8a9603c95c32cef9bac73acd4d0e
Doritoso.o <meyesblue@gmail.com>
2024-05-24 00:11:09 -0400

    uwu #14 //ALLOWHOST

--- apiferremas/settings.py
+++ apiferremas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9wn=v3w$@26qwu_1218*1fl-o*!*8%_7kp+=jzg5l+j=xi-w7^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '3.141.98.183']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '3.141.98.182']
 
 
 # Application definition
