06c8b129e97dfdcdbd4cbc4d7d4ff673ac912b19
Q-Quinn <joaquinpin@outlook.com>
2024-06-04 01:07:01 -0400

    uwu #16 //DATA

--- core/views.py
+++ core/views.py
@@ -233,7 +233,7 @@ def detalle_stock (request,id):
         serializer = StockSerializer(stock, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
